7315aceddbb2f756838c8c40fa041b4a811e2d36
sondermusik <sondermusik@icloud.com>
2024-12-11 13:34:17 +0100

    Initial Commit

---
+++ HotkeyManager/HotkeyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotkeyManagerApp.swift
+//  HotkeyManager
+//
+//  Created by Laurens Karpf on 11.12.24.
+//
+
+import SwiftUI
+
+@main
+struct HotkeyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
